16f26c4d231e6bf5e2ef2b513c757c323ef25b0f
HiZouraiz <officialzouraiz@gmail.com>
2023-06-06 18:27:13 +0500

    Zouraiz changes (6-June-2023).

--- src/app/main/pages/secure/seller/earnings/earnings.component.ts
+++ src/app/main/pages/secure/seller/earnings/earnings.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { colors } from 'app/colors.const';
 
 @Component({
   selector: 'app-earnings',
@@ -10,7 +11,101 @@ export class EarningsComponent implements OnInit {
   constructor() {}
 
   public contentHeader: object;
+  public radioModel = 1;
 
+
+   // Color Variables
+   private successColorShade = 'rgba(115, 103, 240, 0.85)';
+   private tooltipShadow = 'rgba(0, 0, 0, 0.25)';
+   private labelColor = '#6e6b7b';
+   private grid_line_color = 'rgba(200, 200, 200, 0.2)'; // RGBA color helps in dark layout
+ 
+   // ng2-flatpickr options
+   public DateRangeOptions = {
+     altInput: true,
+     mode: 'range',
+     altInputClass: 'form-control flat-picker bg-transparent border-0 shadow-none flatpickr-input',
+     defaultDate: ['2019-05-01', '2019-05-10'],
+     altFormat: 'Y-n-j'
+   };
+ 
+   // Bar Chart
+   public barChart = {
+     chartType: 'bar',
+     datasets: [
+       {
+         data: [275, 90, 190, 205, 125, 85, 55, 87, 127, 150, 230, 280, 190],
+         backgroundColor: this.successColorShade,
+         borderColor: 'transparent',
+         hoverBackgroundColor: this.successColorShade,
+         hoverBorderColor: this.successColorShade
+       }
+     ],
+     labels: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
+     options: {
+       elements: {
+         rectangle: {
+           borderWidth: 2,
+           borderSkipped: 'bottom',
+         }
+       },
+       responsive: true,
+       maintainAspectRatio: false,
+       responsiveAnimationDuration: 500,
+       legend: {
+         display: false
+       },
+       tooltips: {
+         // Updated default tooltip UI
+         shadowOffsetX: 1,
+         shadowOffsetY: 1,
+         shadowBlur: 8,
+         shadowColor: this.tooltipShadow,
+         backgroundColor: colors.solid.white,
+         titleFontColor: colors.solid.black,
+         bodyFontColor: colors.solid.black
+       },
+       scales: {
+         xAxes: [
+           {
+             barThickness: 25,
+             display: true,
+             gridLines: {
+               display: false,
+               color: this.grid_line_color,
+               zeroLineColor: this.grid_line_color
+             },
+             scaleLabel: {
+               display: true
+             },
+             ticks: {
+               fontColor: this.labelColor,
+               fontSize: 13
+ 
+             }
+           }
+         ],
+         yAxes: [
+           {
+             display: false,
+             gridLines: {
+               color: this.grid_line_color,
+               zeroLineColor: this.grid_line_color
+             },
+             ticks: {
+               stepSize: 100,
+               min: 0,
+               max: 400,
+               fontColor: this.labelColor
+             }
+           }
+         ]
+       }
+     },
+     legend: false
+   };
+
+   
   // Lifecycle Hooks
   // -----------------------------------------------------------------------------------------------------
 
