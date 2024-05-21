b095dc9b62f664a089ae58efddd9b27044a30779
HiZouraiz <officialzouraiz@gmail.com>
2024-05-21 16:34:26 +0500

    Fix chat issue.

--- src/app/main/pages/secure/seller/chat/chat-sidebars/chat-user-sidebar-detail/chat-user-sidebar-detail.component.ts
+++ src/app/main/pages/secure/seller/chat/chat-sidebars/chat-user-sidebar-detail/chat-user-sidebar-detail.component.ts
@@ -3,8 +3,10 @@ import { Component, OnInit } from "@angular/core";
 import { CoreSidebarService } from "@core/components/core-sidebar/core-sidebar.service";
 import { ChatService } from "../../chat.service";
 import { environment } from "environments/environment";
+import { NgbModal, NgbModalRef } from "@ng-bootstrap/ng-bootstrap";
 import { UserService } from "@core/services/services/user.service";
 import { Router } from "@angular/router";
+import { OrderService } from "@core/services/services/order.service";
 
 @Component({
   selector: "app-chat-user-sidebar-detail",
@@ -20,6 +22,11 @@ export class ChatUserSidebarDetailComponent implements OnInit {
   public newChat;
   public baseURL = environment.serverURL;
   public chatRoom;
+  public chatUsers;
+  public orderNum;
+  public selectedOrder: any = [];
+  private modalRef: NgbModalRef;
+
   /**
 
   /**
@@ -32,7 +39,9 @@ export class ChatUserSidebarDetailComponent implements OnInit {
     private _chatService: ChatService,
     private _coreSidebarService: CoreSidebarService,
     private userService: UserService,
-    private router: Router
+    private router: Router,
+    private modalService: NgbModal,
+    private orderService: OrderService
   ) {}
 
   // Public Methods
@@ -61,12 +70,66 @@ export class ChatUserSidebarDetailComponent implements OnInit {
    * On init
    */
   searchedSeller;
+
   ngOnInit(): void {
     this._chatService.onSelectedChatUserChange.subscribe((res) => {
       this.chatUser = res;
-      console.log("===============");
-      console.log(res);
-      console.log("===============");
+
+      this._chatService.onSelectedChatChange.subscribe((res) => {
+        this.chatUsers = res;
+      });
+    });
+  }
+
+  orderDetails(modalVC) {
+    if (this.chatUsers) {
+      const orderNum = this.chatUsers.order.orderNum;
+      let queryParams = `?pageSize=10&pageNo=1&sortBy=orderNum&order=asc&orderNum=${orderNum}`;
+      this.orderService
+        .getAllCompleteSellerOrders(queryParams)
+        .subscribe((res) => {
+          this.selectedOrder = res[0].results[0];
+
+          this.modalRef = this.modalService.open(modalVC, {
+            centered: true,
+            size: "lg",
+          });
+
+          this.modalRef.dismissed.subscribe((reason) => {
+            if (reason !== "Cross click") {
+              this.modalService.dismissAll();
+            }
+          });
+        });
+    }
+  }
+
+  getOrderDetails(modalVC) {
+    this._chatService.onSelectedChatChange.subscribe((res) => {
+      this.chatUsers = res;
+      const orderNum = res.order.orderNum;
+
+      let queryParams = `?pageSize=10&pageNo=1&sortBy=orderNum&order=asc&orderNum=${orderNum}`;
+      this.orderService
+        .getAllCompleteSellerOrders(queryParams)
+        .subscribe((res) => {
+          this.openOrderDetailPopup(modalVC, res.results);
+        });
+    });
+  }
+
+  modalOpenVC(modalVC) {
+    this.modalService.open(modalVC, {
+      centered: true,
+      size: "lg",
+    });
+  }
+
+  openOrderDetailPopup(modalVC, selectedOrders) {
+    this.selectedOrder = selectedOrders;
+    this.modalService.open(modalVC, {
+      centered: true,
+      size: "lg",
     });
   }
 
